ca1c5cfcd646cdb60f03f2930b43156ed055df4b
mstivenmendez <maicollstivendm3@hotmail.com>
2025-12-09 19:28:08 -0500

    feat: :sparkles: creacion de la calse cliente y empleado

--- sistema_de_cobros/src/main/java/proyecto/Personal/Cliente.java
+++ sistema_de_cobros/src/main/java/proyecto/Personal/Cliente.java
@@ -1,5 +1,68 @@
 package proyecto.personal;
 
-public class Cliente {
+import java.time.LocalDate;
+
+public class Cliente extends Persona {
+
+   private String idCliente;
+   private String usuario;
+   private String contraseña;
+   private String correo;
+   private String estado;
+   private LocalDate fechaRegistro;
+
+   public Cliente(String nombre, String apellido, String correo, String telefono, String documento,
+         LocalDate fechaNacimiento, String idCliente, String usuario, String contraseña, String correo2, String estado,
+         LocalDate fechaRegistro) {
+      super(nombre, apellido, correo, telefono, documento, fechaNacimiento);
+      this.idCliente = idCliente;
+      this.usuario = usuario;
+      this.contraseña = contraseña;
+      correo = correo2;
+      this.estado = estado;
+      this.fechaRegistro = fechaRegistro;
+   }
+
+   public Cliente() {
+   }
+
+   public String getIdCliente() {
+      return idCliente;
+   }
+   public void setIdCliente(String idCliente) {
+      this.idCliente = idCliente;
+   }
+   public String getUsuario() {
+      return usuario;
+   }
+   public void setUsuario(String usuario) {
+      this.usuario = usuario;
+   }
+   public String getContraseña() {
+      return contraseña;
+   }
+   public void setContraseña(String contraseña) {
+      this.contraseña = contraseña;
+   }
+   public String getCorreo() {
+      return correo;
+   }
+   public void setCorreo(String correo) {
+      this.correo = correo;
+   }
+   public String getEstado() {
+      return estado;
+   }
+   public void setEstado(String estado) {
+      this.estado = estado;
+   }
+   public LocalDate getFechaRegistro() {
+      return fechaRegistro;
+   }
+   public void setFechaRegistro(LocalDate fechaRegistro) {
+      this.fechaRegistro = fechaRegistro;
+   }
+
+
 
 }
